c909a67d9293d2da03c13de5abd69b5cc5f753f1
Mohamed <mohamedjoumoi@gmail.com>
2023-07-05 18:10:50 +0200

    fix: nav button onclick

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -4,18 +4,33 @@ import Logo from "../../components/Logo";
 
 import "./style.scss";
 
+const handleNavClick = (event, sectionClassName) => {
+  event.preventDefault(); // Empêche le comportement par défaut du lien
+
+  const section = document.querySelector(`.${sectionClassName}`);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+};
+
 const Menu = () => (
   <nav>
     <Logo />
     <ul>
       <li>
-        <a href="#nos-services">Nos services</a>
+        <a href="#nos-services" onClick={(event) => handleNavClick(event, "ServicesContainer")}>
+          Nos services
+        </a>
       </li>
       <li>
-        <a href="#nos-realisations">Nos réalisations</a>
+        <a href="#nos-realisations" onClick={(event) => handleNavClick(event, "EventsContainer")}>
+          Nos réalisations
+        </a>
       </li>
       <li>
-        <a href="#notre-equipe">Notre équipe</a>
+        <a href="#notre-equipe" onClick={(event) => handleNavClick(event, "PeoplesContainer")}>
+          Notre équipe
+        </a>
       </li>
     </ul>
     <Button title="contact" onClick={() => (window.document.location.hash = "#contact")}>
